f71ae2efe40b61eca26c3e3626b9014c2c71052c
Christoper <christoper@go-jek.com>
2020-10-24 23:19:00 +0200

    default policy noise, set explr_noise 0.5

--- main.py
+++ main.py
@@ -45,11 +45,11 @@ if __name__ == "__main__":
 	parser.add_argument("--eval_freq", default=1000, type=int)       # How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=50000, type=int)   # Max time steps to run environment
 	parser.add_argument("--n_steps", default=50, type=int)   		# 1 episode how many steps in the environment
-	parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
+	parser.add_argument("--expl_noise", default=0.5)                # Std of Gaussian exploration noise
 	parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
 	parser.add_argument("--discount", default=0.95)                 # Discount factor
 	parser.add_argument("--tau", default=0.005)                     # Target network update rate
-	parser.add_argument("--policy_noise", default=0.5)              # Noise added to target policy during critic update
+	parser.add_argument("--policy_noise", default=0.2)              # Noise added to target policy during critic update
 	parser.add_argument("--noise_clip", default=0.5)                # Range to clip target policy noise
 	parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
 	parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
