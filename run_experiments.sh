c0ebb6c6547a2bea2b497aac7fa7a1ca8319f537
Christoper <christoper@go-jek.com>
2020-11-06 11:08:47 +0100

    add continuous grid world

--- run_experiments.sh
+++ run_experiments.sh
@@ -5,11 +5,14 @@
 for ((i=0;i<3;i+=1))
 do
 	python main.py \
-	--env SparsePendulumRBF-v0 \
+	--env ContinuousGridWorld-v0 \
 	--policy "TD3" \
 	--buffer_size 1000000 \
 	--batch_size 256 \
-	--start_timesteps 1000 \
 	--max_timesteps 20000 \
+	--n_steps 15 \
+	--expl_noise 0.01 \
+	--policy_noise 0.002 \
+	--noise_clip 0.05 \
 	--seed $i
 done
