8afefe1b8a852af061fec7d1a93a316b18e9c4bd
Christoper <christoper@go-jek.com>
2020-11-08 00:08:40 +0100

    change tau 0.5

--- run_experiments.sh
+++ run_experiments.sh
@@ -10,7 +10,7 @@ do
 	--expl_noise 0.5 \
 	--policy_noise 0.2 \
 	--noise_clip 0.5\
-	--tau 1.0 \
+	--tau 0.5 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
@@ -23,7 +23,7 @@ do
 	--expl_noise 0.5 \
 	--policy_noise 0.1 \
 	--noise_clip 0.2\
-	--tau 1.0 \
+	--tau 0.5 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--max_timesteps 5000 \
@@ -35,7 +35,7 @@ do
 	--expl_noise 0.5 \
 	--policy_noise 0.05 \
 	--noise_clip 0.1\
-	--tau 1.0 \
+	--tau 0.5 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--max_timesteps 5000 \
