4b995f021bd1b135a696fb96b81d03d51ae98a7b
Christoper <christoper@go-jek.com>
2020-11-14 01:53:04 +0100

    last effort

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,19 +2,84 @@
 
 # Script to reproduce results
 
-for ((i=0;i<3;i+=1))
+for ((i=0;i<1;i+=1))
 do
 	python main.py \
 	--env ContinuousGridWorld-v0 \
 	--policy "TD3" \
 	--expl_noise 0.01 \
-	--policy_noise 0.2 \
-	--noise_clip 0.5\
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
 	--tau 0.5 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
+	--max_timesteps 5000 \
+	--seed $i
+
+	python main.py \
+	--env ContinuousGridWorld-v0 \
+	--policy "TD3" \
+	--expl_noise 0.01 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.1 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
 	--max_timesteps 20000 \
+	--seed $i
+
+	python main.py \
+	--env ContinuousGridWorld-v0 \
+	--policy "TD3" \
+	--expl_noise 0.01 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.05 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 20000 \
+	--seed $i
+
+	python main.py \
+	--env ContinuousGridWorld-v0 \
+	--policy "TD3" \
+	--expl_noise 0.005 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.5 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 5000 \
+	--seed $i
+
+	python main.py \
+	--env ContinuousGridWorld-v0 \
+	--policy "TD3" \
+	--expl_noise 0.05 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.5 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 5000 \
+	--seed $i
+
+	python main.py \
+	--env ContinuousGridWorld-v0 \
+	--policy "TD3" \
+	--expl_noise 0.1 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.5 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 5000 \
 	--seed $i
 
 done
