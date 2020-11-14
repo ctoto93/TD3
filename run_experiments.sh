6a165ac2497c786bae3f545fe81c4efed1197ae6
Christoper <christoper@go-jek.com>
2020-11-14 20:51:42 +0100

    last 2

--- run_experiments.sh
+++ run_experiments.sh
@@ -10,11 +10,11 @@ do
 	--expl_noise 0.01 \
 	--policy_noise 0.0 \
 	--noise_clip 0.0\
-	--tau 0.5 \
+	--tau 0.05 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
-	--max_timesteps 5000 \
+	--max_timesteps 20000 \
 	--seed $i
 
 	python main.py \
@@ -36,7 +36,7 @@ do
 	--expl_noise 0.01 \
 	--policy_noise 0.0 \
 	--noise_clip 0.0\
-	--tau 0.05 \
+	--tau 0.5 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
@@ -46,40 +46,66 @@ do
 	python main.py \
 	--env ContinuousGridWorld-v0 \
 	--policy "TD3" \
-	--expl_noise 0.005 \
+	--expl_noise 0.01 \
 	--policy_noise 0.0 \
 	--noise_clip 0.0\
-	--tau 0.5 \
+	--tau 1.0 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
-	--max_timesteps 5000 \
+	--max_timesteps 20000 \
 	--seed $i
 
 	python main.py \
-	--env ContinuousGridWorld-v0 \
+	--env SparsePendulumRBF-v0 \
 	--policy "TD3" \
-	--expl_noise 0.05 \
+	--expl_noise 0.5 \
 	--policy_noise 0.0 \
 	--noise_clip 0.0\
-	--tau 0.5 \
+	--tau 0.05 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
-	--max_timesteps 5000 \
+	--max_timesteps 50000 \
 	--seed $i
 
 	python main.py \
-	--env ContinuousGridWorld-v0 \
+	--env SparsePendulumRBF-v0 \
 	--policy "TD3" \
-	--expl_noise 0.1 \
+	--expl_noise 0.5 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.1 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 50000 \
+	--seed $i
+
+	python main.py \
+	--env SparsePendulumRBF-v0 \
+	--policy "TD3" \
+	--expl_noise 0.5 \
 	--policy_noise 0.0 \
 	--noise_clip 0.0\
 	--tau 0.5 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
-	--max_timesteps 5000 \
+	--max_timesteps 50000 \
+	--seed $i
+
+	python main.py \
+	--env SparsePendulumRBF-v0 \
+	--policy "TD3" \
+	--expl_noise 0.5 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 1.0 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 50000 \
 	--seed $i
 
 done
