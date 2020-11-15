c4eae05fe025d92760ec765033c5c2a1f401127a
Christoper <christoper@go-jek.com>
2020-11-15 19:18:00 +0100

    td3_grid_final

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,7 +2,7 @@
 
 # Script to reproduce results
 
-for ((i=0;i<1;i+=1))
+for ((i=0;i<3;i+=1))
 do
 
 	python main.py \
@@ -12,19 +12,6 @@ do
 	--policy_noise 0.0 \
 	--noise_clip 0.0\
 	--tau 0.005 \
-	--buffer_size 1000000 \
-	--batch_size 256 \
-	--start_timesteps 1000 \
-	--max_timesteps 20000 \
-	--seed $i
-
-	python main.py \
-	--env ContinuousGridWorld-v0 \
-	--policy "TD3" \
-	--expl_noise 0.01 \
-	--policy_noise 0.0 \
-	--noise_clip 0.0\
-	--tau 0.001 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
