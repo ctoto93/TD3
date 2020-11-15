7a5ca4d077351fab909ce3e1244d12c62dcd39f2
Christoper <christoper@go-jek.com>
2020-11-15 17:26:52 +0100

    tau 0.001 0.005

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,31 +4,6 @@
 
 for ((i=0;i<1;i+=1))
 do
-	python main.py \
-	--env ContinuousGridWorld-v0 \
-	--policy "TD3" \
-	--expl_noise 0.03 \
-	--policy_noise 0.0 \
-	--noise_clip 0.0\
-	--tau 0.01 \
-	--buffer_size 1000000 \
-	--batch_size 256 \
-	--start_timesteps 1000 \
-	--max_timesteps 20000 \
-	--seed $i
-
-	python main.py \
-	--env ContinuousGridWorld-v0 \
-	--policy "TD3" \
-	--expl_noise 0.01 \
-	--policy_noise 0.0 \
-	--noise_clip 0.0\
-	--tau 0.01 \
-	--buffer_size 1000000 \
-	--batch_size 256 \
-	--start_timesteps 1000 \
-	--max_timesteps 20000 \
-	--seed $i
 
 	python main.py \
 	--env ContinuousGridWorld-v0 \
@@ -42,4 +17,18 @@ do
 	--start_timesteps 1000 \
 	--max_timesteps 20000 \
 	--seed $i
+
+	python main.py \
+	--env ContinuousGridWorld-v0 \
+	--policy "TD3" \
+	--expl_noise 0.01 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.001 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 20000 \
+	--seed $i
+
 done
