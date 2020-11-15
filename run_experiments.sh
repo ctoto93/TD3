398d40b3ca291006549f8c130f2480cef00d99a5
Christoper <christoper@go-jek.com>
2020-11-15 14:35:49 +0100

    tau tuning

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,15 +2,67 @@
 
 # Script to reproduce results
 
-for ((i=0;i<3;i+=1))
+for ((i=0;i<1;i+=1))
 do
+	python main.py \
+	--env SparsePendulumRBF-v0 \
+	--policy "TD3" \
+	--expl_noise 0.5 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 1.0 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 20000 \
+	--seed $i
+
+	python main.py \
+	--env SparsePendulumRBF-v0 \
+	--policy "TD3" \
+	--expl_noise 0.5 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.5 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 20000 \
+	--seed $i
+
+	python main.py \
+	--env SparsePendulumRBF-v0 \
+	--policy "TD3" \
+	--expl_noise 0.5 \
+	--policy_noise 0.0 \
+	--noise_clip 0.0\
+	--tau 0.1 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 20000 \
+	--seed $i
+
 	python main.py \
 	--env SparsePendulumRBF-v0 \
 	--policy "TD3" \
 	--expl_noise 0.5 \
 	--policy_noise 0.2 \
 	--noise_clip 0.5\
-	--tau 0.1 \
+	--tau 0.05 \
+	--buffer_size 1000000 \
+	--batch_size 256 \
+	--start_timesteps 1000 \
+	--max_timesteps 20000 \
+	--seed $i
+
+	python main.py \
+	--env SparsePendulumRBF-v0 \
+	--policy "TD3" \
+	--expl_noise 0.5 \
+	--policy_noise 0.2 \
+	--noise_clip 0.5\
+	--tau 0.01 \
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
