ad0707295de6dd0784ea5e03c42441ae9a07b320
Christoper <christoper@go-jek.com>
2020-11-16 14:22:27 +0100

    td pendulum final noise pn 0 tau 1

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,7 +2,7 @@
 
 # Script to reproduce results
 
-for ((i=0;i<1;i+=1))
+for ((i=0;i<3;i+=1))
 do
 	python main.py \
 	--env SparsePendulumRBF-v0 \
@@ -14,59 +14,7 @@ do
 	--buffer_size 1000000 \
 	--batch_size 256 \
 	--start_timesteps 1000 \
-	--max_timesteps 20000 \
-	--seed $i
-
-	python main.py \
-	--env SparsePendulumRBF-v0 \
-	--policy "TD3" \
-	--expl_noise 0.5 \
-	--policy_noise 0.0 \
-	--noise_clip 0.0\
-	--tau 0.5 \
-	--buffer_size 1000000 \
-	--batch_size 256 \
-	--start_timesteps 1000 \
-	--max_timesteps 20000 \
-	--seed $i
-
-	python main.py \
-	--env SparsePendulumRBF-v0 \
-	--policy "TD3" \
-	--expl_noise 0.5 \
-	--policy_noise 0.0 \
-	--noise_clip 0.0\
-	--tau 0.1 \
-	--buffer_size 1000000 \
-	--batch_size 256 \
-	--start_timesteps 1000 \
-	--max_timesteps 20000 \
-	--seed $i
-
-	python main.py \
-	--env SparsePendulumRBF-v0 \
-	--policy "TD3" \
-	--expl_noise 0.5 \
-	--policy_noise 0.2 \
-	--noise_clip 0.5\
-	--tau 0.05 \
-	--buffer_size 1000000 \
-	--batch_size 256 \
-	--start_timesteps 1000 \
-	--max_timesteps 20000 \
-	--seed $i
-
-	python main.py \
-	--env SparsePendulumRBF-v0 \
-	--policy "TD3" \
-	--expl_noise 0.5 \
-	--policy_noise 0.2 \
-	--noise_clip 0.5\
-	--tau 0.01 \
-	--buffer_size 1000000 \
-	--batch_size 256 \
-	--start_timesteps 1000 \
-	--max_timesteps 20000 \
+	--max_timesteps 50000 \
 	--seed $i
 
 done
